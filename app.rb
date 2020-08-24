fb3dfac0a4fc138a2742fcec97587157fccbf00c
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-24 01:56:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,21 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+
+      erb :new
+    end
+
+    # post '/show' do
+    #   @pirate = Pirate.new(params[:name], params[:weight], params[:height])
+    #   @ship = Ship.new(params[:name], params[:type], params[:booty])
+    #
+    #     erb :show
+    # end
 
   end
 end
